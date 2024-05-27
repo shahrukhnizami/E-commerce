f5293c7d1edfa1be09d5b2504ddfb7a09ab7aaba
shahrukhnizami <nizamishahrukh12@gmail.com>
2024-05-27 14:25:35 +0500

    cadf

--- Productpage/app.js
+++ Productpage/app.js
@@ -1,3 +1,4 @@
+
 var allProducts = [
     {
       "id": 1,
@@ -240,62 +241,26 @@ var allProducts = [
       }
     }
   ]
-var productList=document.getElementById("productList")
-var cartnumber=document.getElementById("cartnumber")
 
-var cardbtn=document.getElementsByClassName("cardbtn")
-var like=document.getElementsByClassName("like")
-  for(var i=0; i<allProducts.length;i++){
-    
+  //imageP.src = allProducts[2].image
+  document.getElementById("image").src= allProducts[2].image
+  document.getElementById("p_Title").innerText= allProducts[2].title
+  document.getElementById("discrip").innerText= allProducts[2].description
+  document.getElementById("price").innerText= allProducts[2].price
+  document.getElementById("categ").innerText= allProducts[2].category
+  document.getElementById("rate").innerText=parseFloat(allProducts[2].rating.rate)
 
-    var product =`<div class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
-    <a href="#">
-        <img class="hover:grow hover:shadow-lg" src="${allProducts[i].image}">
-        <div class="pt-3 flex items-center justify-between">
-            <p class="">${allProducts[i].title}</p>
-          <svg  class="h-6 w-6 like fill-current text-gray-500 hover:text-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path d="M12,4.595c-1.104-1.006-2.512-1.558-3.996-1.558c-1.578,0-3.072,0.623-4.213,1.758c-2.353,2.363-2.352,6.059,0.002,8.412 l7.332,7.332c0.17,0.299,0.498,0.492,0.875,0.492c0.322,0,0.609-0.163,0.792-0.409l7.415-7.415 c2.354-2.354,2.354-6.049-0.002-8.416c-1.137-1.131-2.631-1.754-4.209-1.754C14.513,3.037,13.104,3.589,12,4.595z M18.791,6.205 c1.563,1.571,1.564,4.025,0.002,5.588L12,18.586l-6.793-6.793C3.645,10.23,3.646,7.776,5.205,6.209 c0.76-0.756,1.754-1.172,2.799-1.172s2.035,0.416,2.789,1.17l0.5,0.5c0.391,0.391,1.023,0.391,1.414,0l0.5-0.5 C14.719,4.698,17.281,4.702,18.791,6.205z" />
-            </svg>
-        </div>
-        <p class="pt-1 text-gray-900">${allProducts[i].price}</p>
-        <button id="add"  id="cart"class=" cardbtn  rounded-md border px-7 py-3 text-center text-black font-medium text-white">
-        Add
-        </button>
-    </a>
-</div>`
-
- productList.innerHTML += product;
-
+  function like(elemet){
+    //console.log(elemet.parentElement.value)
+     if(elemet.parentElement.value =="red"){
+        elemet.style.color = 'red'
+        elemet.parentElement.style.border = '1px solid black'
+        elemet.parentElement.value="black"
+     }
+     else if(elemet.parentElement.value =="black"){
+        elemet.style.color = 'darkgrey'
+        elemet.parentElement.value="red"
+        elemet.parentElement.style.border = 'none'
+     }
   }
-
-  for(var i=0;i<cardbtn.length;i++)
-
-{ 
- cardbtn[i].addEventListener('click',function () {
-  if(this.innerText=="Added")
-  {cartnumber.innerText= cartnumber.innerText - 1
-    this.innerText="Add"
-    this.style.background= 'darkgrey'
-    
-  }
-else{
   
-  cartnumber.innerText=parseInt(cartnumber.innerText) + 1
-  this.innerText= "Added" 
-  this.style.background= 'red'
-} })
-}
-
-for(var i=0;i<like.length;i++){
-  like[i].addEventListener('click',function(){
-    this.style.color='red'
-  })
-  like[i].addEventListener('dblclick',function(){
-    this.style.color='black'
-  })
-}
-
-//document.getElementById("image").src = allProducts[1].image
-var image1 = document.getElementById("image")
-image1.src = allProducts[1].image 
-console.log(image1)
